981dcac8c3e83e4038f9f51b41722711ccaecdfd
MaxSvargal <maxsvargal@gmail.com>
2013-12-14 01:56:19 +0400

    Init commit

---
+++ node-auto-purge-os-x.js
@@ -0,0 +1,22 @@
+var os = require('os'),
+    exec = require('child_process').exec;
+
+var intervalSec = 60,
+    memLimitMb = 300;
+
+(function() {
+  var calculate = function() {
+    var freeMem = os.freemem() / (1024 * 1024);
+    if (freeMem <= memLimitMb) {
+      console.log("Low memory. Start purge command...");
+      return exec('purge', function(err, stdout, stderr) {
+        return console.log("Purged from " + freeMem + " Mb");
+      });
+    } else {
+      return console.log("Free memory is " + freeMem + " Mb");
+    }
+  };
+  console.log("Script started with a limit " + memLimitMb + " Mb of memory for purge and with check interval " + intervalSec + " seconds.");
+  setInterval(calculate, intervalSec * 1000);
+  calculate();
+})()
