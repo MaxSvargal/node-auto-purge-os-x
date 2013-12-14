5c0b90bd423d98782521a0a892ec63b53855e795
MaxSvargal <maxsvargal@gmail.com>
2013-12-14 23:33:39 +0400

    Add colors and time

--- node-auto-purge-os-x.js
+++ node-auto-purge-os-x.js
@@ -5,18 +5,37 @@ var intervalSec = 60,
     memLimitMb = 200;
 
 (function() {
+  var log = function(str) {
+    return console.log( '\x1B[90m[' + getTime() + ']\x1B[39m ' + str);
+  };
+
+  var getFreeMem = function() {
+    return Math.round(os.freemem() / (1024 * 1024));
+  };
+
+  var getTime = function() {
+    d = new Date();
+    return [d.getHours(), d.getMinutes(), d.getSeconds()].join(':');
+  };
+
+  var execPurge = function() {
+    exec('purge', function(err, stdout, stderr) {
+      log("\x1B[33mPurged from " + getFreeMem() + " Mb\x1B[39m");
+    });
+  };
+
   var calculate = function() {
-    var freeMem = os.freemem() / (1024 * 1024);
+    var freeMem = getFreeMem();
     if (freeMem <= memLimitMb) {
-      console.log("Low memory. Start purge command...");
-      return exec('purge', function(err, stdout, stderr) {
-        return console.log("Purged from " + freeMem + " Mb");
-      });
+      log("\x1B[32mLow memory (" + freeMem + " Mb). Start purge command...\x1B[39m");
+      execPurge();
     } else {
-      return console.log("Free memory is " + freeMem + " Mb");
+      log("Free memory is \x1B[1m" + freeMem + " Mb\x1B[22m");
     }
   };
-  console.log("Script started with a limit " + memLimitMb + " Mb of memory for purge and with check interval " + intervalSec + " seconds.");
+
+  log("\x1B[36mScript started with a limit " + memLimitMb + " Mb of memory for purge and with check interval " + intervalSec + " seconds.\x1B[39m");
   setInterval(calculate, intervalSec * 1000);
   calculate();
+  
 })()
