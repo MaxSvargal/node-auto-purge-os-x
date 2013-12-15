6819dc8e9d5ce99d47e1931d84d36b43d7c87a03
MaxSvargal <maxsvargal@gmail.com>
2013-12-15 12:00:13 +0400

    Refactoring of colors

--- node-auto-purge-os-x.js
+++ node-auto-purge-os-x.js
@@ -4,6 +4,23 @@ var os = require('os'),
 var intervalSec = 60,
     memLimitMb = 200;
 
+var AddConsoleStyles = (function() {
+  var addProperty = function(color, openTag, closeTag) {
+    String.prototype.__defineGetter__(color, function() {
+      return openTag + this + closeTag;
+    });
+  };
+  var styles = {
+    'cyan'  : ['\x1B[36m', '\x1B[39m'],
+    'green' : ['\x1B[32m', '\x1B[39m'],
+    'yellow': ['\x1B[33m', '\x1B[39m'],
+    'bold'  : ['\x1B[1m', '\x1B[22m']
+  };
+  for(style in styles) {
+    addProperty(style, styles[style][0], styles[style][1]);
+  };
+})();
+
 (function() {
   var log = function(str) {
     return console.log( '\x1B[90m[' + getTime() + ']\x1B[39m ' + str);
@@ -20,22 +37,23 @@ var intervalSec = 60,
 
   var execPurge = function(preFreeMem) {
     exec('purge', function(err, stdout, stderr) {
-      log("\x1B[33mPurged " + (getFreeMem() - preFreeMem) + " Mb\x1B[39m");
+      log(("Purged " + (getFreeMem() - preFreeMem) + " Mb").yellow);
     });
   };
 
   var calculate = function() {
     var freeMem = getFreeMem();
     if (freeMem <= memLimitMb) {
-      log("\x1B[32mLow memory (" + freeMem + " Mb). Start purge command...\x1B[39m");
+      log(("Low memory (" + freeMem + " Mb). Start purge command...\x1B[39m").green);
       execPurge(freeMem);
     } else {
-      log("\x1B[1m" + freeMem + " Mb\x1B[22m free.");
+      log(freeMem.toString().bold + " Mb free.");
     }
   };
 
-  log("\x1B[36mScript started with a limit " + memLimitMb + "Mb of memory for purge and with check interval " + intervalSec + " seconds.\x1B[39m");
+  log(("Script started with a limit " + memLimitMb + "Mb of memory for purge and with check interval " + intervalSec + " seconds.").cyan);
   setInterval(calculate, intervalSec * 1000);
   calculate();
 
 })()
+
